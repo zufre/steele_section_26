dc6642b917ba5ad02c84ee3c1fa2d4c2e81208d2
User <user@Mac.local>
2018-05-06 00:49:52 +1000

    added responisive layout to landing page

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -9,7 +9,17 @@ app.set('view engine', 'ejs');
 var campgrounds =[
   {name: 'Salomon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
   {name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'},
+  {name: 'Mountain Goat\'s Rest', image: 'https://farm4.staticflickr.com/3751/9580653400_e1509d6696.jpg'},
+  {name: 'Salomon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
+  {name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'},
+  {name: 'Mountain Goat\'s Rest', image: 'https://farm4.staticflickr.com/3751/9580653400_e1509d6696.jpg'},
+  {name: 'Salomon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
+  {name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'},
+  {name: 'Mountain Goat\'s Rest', image: 'https://farm4.staticflickr.com/3751/9580653400_e1509d6696.jpg'},
+  {name: 'Salomon Creek', image: 'https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg'},
+  {name: 'Granite Hill', image: 'https://farm4.staticflickr.com/3273/2602356334_20fbb23543.jpg'},
   {name: 'Mountain Goat\'s Rest', image: 'https://farm4.staticflickr.com/3751/9580653400_e1509d6696.jpg'}
+
   ]
 
 app.get('/', function(req, res) {
@@ -35,4 +45,4 @@ app.get('/campgrounds/new', function(req, res) {
 
 app.listen(3000,  process.env.IP, function() {
   console.log('The YelpCamp server has started');
-});
+})
